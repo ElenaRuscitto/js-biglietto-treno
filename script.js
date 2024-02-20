162a7939e5cd8d499ca34dfb1f3882f810880d21
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-02-20 15:21:39 +0100

    [add] 3

---
+++ script.js
@@ -0,0 +1,9 @@
+const kmPercorrere = parseFloat (prompt ('Quanti km dei percorrere?'));
+
+const anni = parseInt (prompt ('Quanti anni hai?'));
+
+
+const costoKm = 0.21;
+
+const prezzoBiglietto = kmPercorrere * costoKm;
+console.log (kmPercorrere, anni, prezzoBiglietto);
