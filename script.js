4e5cf95361e78e10ca57767b5e8524c85e80617f
ElenaRuscitto <ruscitto.elena@gmail.com>
2024-02-20 15:54:11 +0100

    [add] sconti

--- script.js
+++ script.js
@@ -5,5 +5,17 @@ const anni = parseInt (prompt ('Quanti anni hai?'));
 
 const costoKm = 0.21;
 
-const prezzoBiglietto = kmPercorrere * costoKm;
+let prezzoBiglietto = kmPercorrere * costoKm;
 console.log (kmPercorrere, anni, prezzoBiglietto);
+
+if (anni <= 17) {
+  let scontoMin = prezzoBiglietto * 20 / 100
+  prezzoBiglietto = prezzoBiglietto - scontoMin
+}
+
+if (anni >= 65) {
+  let scontoMin = prezzoBiglietto * 40 / 100
+  prezzoBiglietto = prezzoBiglietto - scontoMin
+}
+
+console.log (prezzoBiglietto.toFixed(2))
